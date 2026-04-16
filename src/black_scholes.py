4566986117bfe3b33790bfbe3ed7db3b1c82b17f
willy-guilleminot <willy.guilleminot@essec.edu>
2026-04-17 01:10:58 +0200

    Add comments in monte_carlo.py and black_scholes.py

--- src/black_scholes.py
+++ src/black_scholes.py
@@ -1,60 +1,67 @@
 import numpy as np
 from scipy.stats import norm
 
-def _d1_d2(S, K, T, r, sigma):
+# Compute d1 and d2
+def _d1_d2(S: float, K: float, T: float, r: float, sigma: float) -> tuple[float, float]:
     d1 = (np.log(S / K) + (r + sigma**2 / 2) * T) / (sigma * np.sqrt(T))
     d2 = d1 - sigma * np.sqrt(T)
     return d1, d2
 
-def call_price(S, K, T, r, sigma):
+# Compute Black-Sholes call price giving the spot, the strike, the maturity, the interest rate, and the volatility
+def call_price(S: float, K: float, T: float, r: float, sigma: float) -> float:
     d1, d2 = _d1_d2(S, K, T, r, sigma)
     return norm.cdf(d1) * S - norm.cdf(d2) * K * np.exp(-r * T)
 
-def put_price(S, K, T, r, sigma):
+# Compute Black-Sholes put price giving the spot, the strike, the maturity, the interest rate, and the volatility
+def put_price(S: float, K: float, T: float, r: float, sigma: float) -> float:
     d1, d2 = _d1_d2(S, K, T, r, sigma)
     return norm.cdf(-d2) * K * np.exp(-r * T) - norm.cdf(-d1) * S
 
-def delta_call(S, K, T, r, sigma):
+# Compute the call delta (partial derivative of the call price with respect to the underlying price)
+def delta_call(S: float, K: float, T: float, r: float, sigma: float) -> float:
     d1, _ = _d1_d2(S, K, T, r, sigma)
     return norm.cdf(d1)
 
-def delta_put(S, K, T, r, sigma):
+# Compute the put delta (partial derivative of the put price with respect to the underlying price)
+def delta_put(S: float, K: float, T: float, r: float, sigma: float) -> float:
     d1, _ = _d1_d2(S, K, T, r, sigma)
     return norm.cdf(d1) - 1
 
-def gamma(S, K, T, r, sigma):
+# Compute the put delta (second partial derivative of the option price with respect to the underlying price)
+def gamma(S: float, K: float, T: float, r: float, sigma: float) -> float:
     d1, _ = _d1_d2(S, K, T, r, sigma)
     return norm.pdf(d1) / (np.sqrt(T) * S * sigma)
 
-def vega(S, K, T, r, sigma):
+# Compute the vega (partial derivative of the option price with respect to the underlying volatility)
+def vega(S: float, K: float, T: float, r: float, sigma: float) -> float:
     d1, _ = _d1_d2(S, K, T, r, sigma)
     return S * norm.pdf(d1) * np.sqrt(T)
 
-def theta_call(S, K, T, r, sigma):
+# Compute the call theta (partial derivative of the call price with respect of the ...)
+def theta_call(S: float, K: float, T: float, r: float, sigma: float) -> float:
     d1, d2 = _d1_d2(S, K, T, r, sigma)
     return -S * norm.pdf(d1) * sigma / (2 * np.sqrt(T)) - r * K * np.exp(-r * T) * norm.cdf(d2)
 
-def theta_put(S, K, T, r, sigma):
+# Compute the put theta (partial derivative of the put price with respect of the ...)
+def theta_put(S: float, K: float, T: float, r: float, sigma: float) -> float:
     d1, d2 = _d1_d2(S, K, T, r, sigma)
     return -S * norm.pdf(d1) * sigma / (2 * np.sqrt(T)) + r * K * np.exp(-r * T) * norm.cdf(d2)
 
-def rho_call(S, K, T, r, sigma):
+# Compute the call rho (partial derivative of the call price with respect of the interest rate)
+def rho_call(S: float, K: float, T: float, r: float, sigma: float) -> float:
     _, d2 = _d1_d2(S, K, T, r, sigma)
     return K * T * np.exp(-r * T) * norm.cdf(d2)
 
-def rho_put(S, K, T, r, sigma):
+# Compute the put rho (partial derivative of the put price with respect of the interest rate)
+def rho_put(S: float, K: float, T: float, r: float, sigma: float) -> float:
     _, d2 = _d1_d2(S, K, T, r, sigma)
     return -K * T * np.exp(-r * T) * norm.cdf(-d2)
 
-def _bisection_method(price, S, K, T, r, option_type, epsilon, max_iter):
+# Use the bisection method to the root of function's root (implie volatility)
+def _bisection_method(price: float, S: float, K: float, T: float, r: float, pricer: function, epsilon: float, max_iter: int) -> float:
     volLower = 0.001
     volUpper = 10
     volMid = (volLower + volUpper) / 2
-
-    if option_type=='call':
-        pricer = call_price
-    else:
-        pricer = put_price
 
     for _ in range(max_iter):
         if abs(pricer(S, K, T, r, volMid) - price) > epsilon:
@@ -68,13 +75,9 @@ def _bisection_method(price, S, K, T, r, option_type, epsilon, max_iter):
     
     raise ValueError("Bisection did not converge")
 
-def _newton_raphson(price, S, K, T, r, option_type, epsilon, guess, max_iter):
+# Use the Newton method to find the function's root (implied volatility)
+def _newton_raphson(price: float, S: float, K: float, T: float, r: float, pricer: function, epsilon: float, guess: float, max_iter: int) -> float:
     sigma = guess
-
-    if option_type == 'call':
-        pricer = call_price
-    else:
-        pricer = put_price
 
     for _ in range(max_iter):
         if abs(pricer(S, K, T, r, sigma) - price) > epsilon:
@@ -84,8 +87,22 @@ def _newton_raphson(price, S, K, T, r, option_type, epsilon, guess, max_iter):
     
     raise ValueError("Newton-Raphson did not converge")
 
-def implied_volatility(price, S, K, T, r, option_type='call', method='bisection', epsilon=10**(-5), guess=0.2, max_iter=1000):
-    if method == 'bisection':
-        return _bisection_method(price, S, K, T, r, option_type, epsilon, max_iter)
+# Giving the Black Scholes parameters and the option's price, finds the option's implied volatility 
+# using either the newton method or the bisection method 
+def implied_volatility(price: float, S: float, K: float, T: float, r: float, option_type: str = 'call', method: str = 'bisection', epsilon: float = 10**(-5), guess: float = 0.2, max_iter: int = 1000) -> float:
+    if option_type == 'call':
+        pricer = call_price
+    elif option_type == 'put':
+        pricer = put_price
     else:
-        return _newton_raphson(price, S, K, T, r, option_type, epsilon, guess, max_iter)
+        raise ValueError("Option type not recognized. Please use 'call' or 'put'.")
+
+    if type(max_iter) != int:
+        raise ValueError("max_iter type not valid. Please provide an integer.")
+    
+    if method == 'bisection':
+        return _bisection_method(price, S, K, T, r, pricer, epsilon, max_iter)
+    elif method == 'newton':
+        return _newton_raphson(price, S, K, T, r, pricer, epsilon, guess, max_iter)
+    else:
+        raise ValueError("Wrong method provided. Please use 'bisection' or 'newton'.")
--- src/monte_carlo.py
+++ src/monte_carlo.py
@@ -1,12 +1,14 @@
 import numpy as np
 
+# Use Monte Carlo to find the call price
 def mc_call_price(S, K, T, r, sigma, n_simulations=10000):
-    Z = np.random.standard_normal(n_simulations)
-    S_T = S *  np.exp((r - sigma**2 / 2) * T + sigma * np.sqrt(T) * Z)
-    payoffs = np.maximum(S_T - K, 0)
+    Z = np.random.standard_normal(n_simulations) # Provides a vector of n_simulations draw from the standard normal distribution
+    S_T = S *  np.exp((r - sigma**2 / 2) * T + sigma * np.sqrt(T) * Z) # Compute the vector of spot prices at maturity date
+    payoffs = np.maximum(S_T - K, 0) # Vector of payoffs
 
     return np.mean(payoffs) * np.exp(-r * T)
 
+# Use Monte Carlo to find the put price
 def mc_put_price(S, K, T, r, sigma, n_simulations=10000):
     Z = np.random.standard_normal(n_simulations)
     S_T = S *  np.exp((r - sigma**2 / 2) * T + sigma * np.sqrt(T) * Z)
@@ -14,6 +16,7 @@ def mc_put_price(S, K, T, r, sigma, n_simulations=10000):
 
     return np.mean(payoffs) * np.exp(-r * T)
 
+# Use Monte Carlo to find the call price by minimizing the calculation cost using the...
 def mc_call_price_antithetic(S, K, T, r, sigma, n_simulations=10000):
     Z = np.random.standard_normal(n_simulations // 2)
 
@@ -25,6 +28,7 @@ def mc_call_price_antithetic(S, K, T, r, sigma, n_simulations=10000):
 
     return np.mean((payoffs_1 + payoffs_2) / 2) * np.exp(-r * T)
 
+# Use Monte Carlo to find the put price by minimizing the calculation cost using the...
 def mc_put_price_antithetic(S, K, T, r, sigma, n_simulations=10000):
     Z = np.random.standard_normal(n_simulations // 2)
 
@@ -36,23 +40,27 @@ def mc_put_price_antithetic(S, K, T, r, sigma, n_simulations=10000):
 
     return np.mean((payoffs_1 + payoffs_2) / 2) * np.exp(-r * T)
 
+# Provides a n_simulations x n_steps vector of paths 
 def simulate_paths(S, T, r, sigma, n_simulations, n_steps):
     dt = T / n_steps
-    paths = np.zeros((n_simulations, n_steps + 1))
-    paths[:, 0] = S
+    paths = np.zeros((n_simulations, n_steps + 1)) # Create a n_simulations x n_steps vector filled with zeros
+    paths[:, 0] = S # Initialize the spot price at t=0
 
+    # Compute the spot price at each steps using Brownian Motion
     for t in range(1, n_steps + 1):
         Z = np.random.standard_normal(n_simulations)
         paths[:, t] = paths[:, t - 1] * np.exp((r - sigma**2 / 2) * dt + sigma * np.sqrt(dt) * Z)
 
     return paths
 
+# Compute asian call price using Monte Carlo
 def mc_asian_call(S, K, T, r, sigma, n_simulations=10000, n_steps=252):
     paths = simulate_paths(S, T, r, sigma, n_simulations, n_steps)
     payoffs = np.maximum(paths.mean(axis=1) - K, 0)
 
     return np.mean(payoffs) * np.exp(-r * T)
 
+# Compute call up and out price using Monte Carlo
 def mc_barrier_call(S, K, T, r, sigma, barrier, n_simulations=10000, n_steps=252):
     paths = simulate_paths(S, T, r, sigma, n_simulations, n_steps)
     barrier_crossed = np.max(paths, axis=1) >= barrier
